65db91caa205c86ffa1c9631445910ea1a12e98a
Wesley Carlsen <wesley.carlsen@redpillanalytics.com>
2016-08-07 16:38:41 -0400

    Add basic javafx application window

--- src/com/sonder/Sonder.java
+++ src/com/sonder/Sonder.java
@@ -1,10 +1,38 @@
 package com.sonder;
 
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
 /**
  * Created by wesley on 8/7/16.
  */
-public class Sonder {
+public class Sonder extends Application {
+
     public static void main(String[] args) {
-        System.out.println("Hello");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Hello World!");
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
     }
 }
